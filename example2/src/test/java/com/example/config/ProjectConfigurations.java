c18ed6be333dc1424ba8f1f9837bfef667726e9b
Kapilesh Rajput <rajputkapilesh@outlook.com>
2025-10-19 11:44:34 +0530

    Create another project to continue learning

---
+++ example2/src/test/java/com/example/config/ProjectConfigurations.java
@@ -0,0 +1,39 @@
+package com.example.config;
+
+import com.example.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*
+Spring @Configuration annotation is part of the Spring core framework.
+Spring Configuration annotation indicates that the class has @Bean definition
+methods. So, spring container can process the class and generate spring Beans to be
+used in the application.
+*/
+@Configuration
+public class ProjectConfigurations {
+
+    /*
+    NOTE:
+    The methods names will determine the name of the spring bean in the IOC context.
+
+    @Bean annotation, which lets spring know that it needs to call this method when
+    it initializes its context and adds the returned value to the context.
+    */
+    @Bean
+    Vehicle vehicle() {
+        var vehicle = new Vehicle();
+        vehicle.setName("Audi 8");
+        return vehicle;
+    }
+
+    @Bean
+    String hello() {
+        return "Hello World";
+    }
+
+    @Bean
+    Integer number() {
+        return 16;
+    }
+}
