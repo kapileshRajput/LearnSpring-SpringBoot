296af23b1c2db24b2ca8e60cc19d4def38a550d1
Kapilesh Rajput <rajputkapilesh@outlook.com>
2025-10-19 12:05:15 +0530

    Create new project

---
+++ example3/src/test/java/com/example/main/Example3.java
@@ -0,0 +1,25 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfigurations;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example3 {
+
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("Honda City");
+        System.out.println("Vehicle name from non spring context is: " + vehicle.getName());
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfigurations.class);
+
+        Vehicle vehicle1 = context.getBean("vehicle1", Vehicle.class);
+        System.out.println("Vehicle name from spring context is: " + vehicle1.getName());
+
+        Vehicle vehicle2 = context.getBean("vehicle2", Vehicle.class);
+        System.out.println("Vehicle name from spring context is: " + vehicle2.getName());
+
+        Vehicle vehicle3 = context.getBean("vehicle3", Vehicle.class);
+        System.out.println("Vehicle name from spring context is: " + vehicle3.getName());
+    }
+}
